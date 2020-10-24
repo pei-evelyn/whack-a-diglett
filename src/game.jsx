866bf25cb461bafc0c0ee5fd047815f9610c679f
connor-ashlock <connor9804@yahoo.com>
2020-10-24 19:31:18 +0000

    added background image

---
+++ src/game.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="game-background">
+        <div className=""></div>
+      </div>
+    )
+  }
+}
+
+export default Game;
