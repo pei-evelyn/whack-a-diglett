1f0014db4557984eb2dcea02b3f3a4b9beafcbbd
connor-ashlock <connor9804@yahoo.com>
2020-10-25 03:10:45 +0000

    added diglett to screen and ability to click him

--- src/game.jsx
+++ src/game.jsx
@@ -3,12 +3,29 @@ import React from 'react';
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    this.handleDiglettClick = this.handleDiglettClick.bind(this);
+    this.state = { isHit: false, isHere: true };
+  }
+
+  handleDiglettClick() {
+    this.setState({ isHit: true });
+    setTimeout(() => {
+      this.setState({ isHere: false });
+    }, 2000);
   }
 
   render() {
+    if (!this.state.isHere) {
+      return (
+        <div className="diglett-container"></div>
+      )
+    }
     return (
-      <div className="game-background">
-        <div className=""></div>
+      <div className="diglett-container">
+        { this.state.isHit
+        ? <img className="diglett-angry" src="images/diglett-angry.png" />
+        : <img className="diglett" src="images/diglett.png" onClick={this.handleDiglettClick} />
+        }
       </div>
     )
   }
