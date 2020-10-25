ed219844fda67ff7ac4e50db5ac4cd2e24e88e95
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-25 14:07:35 -0700

    Added Start component and start screen

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import Game from './game';
+import Start from './start';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isStarted: false,
+      player: null
+    };
+    this.startGame = this.startGame.bind(this);
+  }
+
+  startGame(player) {
+    console.log(player)
+    this.setState(state => ({
+      isStarted: !state.isStarted,
+      player: player
+    }))
   }
 
   render() {
+    if (!this.state.isStarted) {
+      return (
+        <Start startGame={this.startGame}/>
+      )
+    }
+
     return (
       <div className="game-background">
         <Game />
