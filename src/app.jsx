a87e2c41fdd8e8bda88647951e53bffbd04af157
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-25 17:18:09 -0700

    Added score component

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Game from './game';
 import Start from './start';
+import Score from './score';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,7 +23,12 @@ class App extends React.Component {
   render() {
     return !this.state.isStarted ?
       <Start startGame={this.startGame} /> :
-      <div className={`game-background ${this.state.gender}`}><Game /></div>
+      (
+        <div className={`game-background ${this.state.gender}`}>
+          <Score />
+          <Game />
+        </div>
+      )
   }
 }
 
---
+++ src/score.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Score extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Score;
