7d182cac25db64af63af6e82947f162b0cff75e2
MattChen1021 <mattchen1021gmail.com>
2020-10-28 02:07:52 +0000

    sound button works for start screen

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import Start from './start';
 import Timer from './timer'
 import Score from './score';
 import Health from './health';
+import ReactAudioPlayer from 'react-audio-player';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,11 +13,14 @@ class App extends React.Component {
       isStarted: false,
       gender: null,
       hits: 0,
-      seconds: 45
+      seconds: 45,
+      paused: true
     };
     this.startGame = this.startGame.bind(this);
     this.increaseHits = this.increaseHits.bind(this);
-    this.startCountdown = this.startCountdown.bind(this)
+    this.startCountdown = this.startCountdown.bind(this);
+    this.playTitleMusic = this.playTitleMusic.bind(this);
+    this.titleMusic = new Audio('music/title-screen.ogg');
   }
 
   startGame(gender) {
@@ -25,6 +29,20 @@ class App extends React.Component {
       gender: gender,
       hits: 0,
       seconds: 45
+    }))
+  }
+
+  playTitleMusic() {
+    const musicState = this.state.paused
+    if (musicState) {
+      this.titleMusic.play();
+      this.titleMusic.loop = true;
+    } else {
+      this.titleMusic.paused;
+      this.titleMusic.pause();
+    }
+    this.setState(state => ({
+      paused: !state.paused
     }))
   }
 
@@ -46,30 +64,37 @@ class App extends React.Component {
 
   render() {
     if (!this.state.isStarted) {
-      return <Start startGame={this.startGame} />
+      return (
+        <Start
+          startGame={this.startGame}
+          startMusic={this.playTitleMusic}
+          musicState={this.state.paused}
+        />)
     }
     return (
-      <div className={`game-background ${this.state.gender}`}>
-        <div className="diglett-name">Diglett</div>
-        <div className="diglett-lvl">9</div>
-        <div className="trainer-name">Trainer</div>
-        <div className="trainer-lvl">62</div>
-        <div className="trainer-atr">114 114</div>
-        <Score score={this.state.hits} />
-        <Health />
-        <Game
-          increaseHits={this.increaseHits}
-          seconds={this.state.seconds}
-          countdown={this.startCountdown}
-        />
-        <Timer
-          restartGame={this.startGame}
-          openModal={this.openModal}
-          isOpen={this.state.isOpen}
-          score={this.state.hits}
-          seconds={this.state.seconds}
-        />
-      </div>
+      <>
+        <div className={`game-background ${this.state.gender}`}>
+          <div className="diglett-name">Diglett</div>
+          <div className="diglett-lvl">9</div>
+          <div className="trainer-name">Trainer</div>
+          <div className="trainer-lvl">62</div>
+          <div className="trainer-atr">114 114</div>
+          <Score score={this.state.hits} />
+          <Health />
+          <Game
+            increaseHits={this.increaseHits}
+            seconds={this.state.seconds}
+            countdown={this.startCountdown}
+          />
+          <Timer
+            restartGame={this.startGame}
+            openModal={this.openModal}
+            isOpen={this.state.isOpen}
+            score={this.state.hits}
+            seconds={this.state.seconds}
+          />
+        </div>
+      </>
     )
   }
 }
--- src/start.jsx
+++ src/start.jsx
@@ -2,29 +2,42 @@ import React from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
 const Start = props => {
+  const musicPlaying = props.musicState;
+  const soundButton = props => {
+    if (musicPlaying) {
+      return <button className="choose-button p-2" onClick={props.startMusic}>Sound On</button>
+    } else {
+      return <button className="choose-button p-2" onClick={props.startMusic}>Sound Off</button>
+    }
+  }
   return (
-    <div className="start-screen container">
-      <div className="start-title row">
-        <h4>Whack-a-Diglett</h4>
-        <h6>Choose your player:</h6>
-      </div>
-      <div className="row button-row">
-        <div className="col">
-          <button
-            className="choose-button"
-            onClick={() => props.startGame('female')}>
-            Select
-          </button>
+    <>
+      <div className="start-screen container">
+        <div className="start-title row">
+          <h4>Whack-a-Diglett</h4>
+          <h6>Choose your player:</h6>
         </div>
-        <div className="col">
-          <button
-            className="choose-button"
-            onClick={() => props.startGame('male')}>
-            Select
+        <div className="row button-row">
+          <div className="col">
+            <button
+              className="choose-button"
+              onClick={() => props.startGame('female')}>
+              Select
           </button>
+          </div>
+          <div className="col">
+            <button
+              className="choose-button"
+              onClick={() => props.startGame('male')}>
+              Select
+          </button>
+          </div>
+        </div>
+        <div className="row mt-5 button-row">
+          {soundButton(props)}
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
